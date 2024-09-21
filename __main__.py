165d5466b2ee699069c8186d0c8c51781d98d454
kalitka <zavorotnii.dr@gmail.com>
2024-09-21 13:32:45 +0700

    Add argument parsing to __main__

--- __main__.py
+++ __main__.py
@@ -1,3 +1,26 @@
-from converter import TextBlazeToEspansoConverter
+import os.path
 
-TextBlazeToEspansoConverter().convert()
+from dotenv import load_dotenv
+
+from textblaze_to_espanso import TextBlazeToEspansoConverter
+import argparse
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument(
+        "--env_file", type=str, help="Path to env file for the package "
+    )
+    args = parser.parse_args()
+    if args.env_file:
+        if os.path.exists(args.env_file):
+            load_dotenv(args.env_file)
+            print(f"Loaded environment variables from {args.env_file}")
+        else:
+            print(f"File {args.env_file} does not exist.")
+            exit(1)
+
+
+if __name__ == "__main__":
+    parse_arguments()
+    TextBlazeToEspansoConverter().convert()
